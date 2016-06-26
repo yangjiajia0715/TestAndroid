8ad515e2c910a3672a5fe26152433b2e0adda199
yangjiajia <875880715@qq.com>
2016-06-26 21:49:47 +0800

    gongsi cmp

--- src/com/example/testandroid/MainActivity.java
+++ src/com/example/testandroid/MainActivity.java
@@ -1,26 +1,29 @@
 package com.example.testandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.tv_test_nothing).setOnClickListener(this);
 
         //test android
         //yangjiajia 家里电脑 日志 2016年6月26日19:49:18
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.tv_test_nothing:
+                break;
+        }
     }
-    
 }
