b47f8b9d04fee7eacf7f168bde4848584d9c156a
yangjiajia-home <576125754@qq.com>
2016-07-27 23:31:17 +0800

    已经可以缩放了

--- src/com/example/testandroid/activity/ZoomImage.java
+++ src/com/example/testandroid/activity/ZoomImage.java
@@ -2,9 +2,13 @@ package com.example.testandroid.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
@@ -15,23 +19,35 @@ import android.widget.ImageView;
  * description:
  * modify time: 2016/7/17 14:54
  */
-public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener {
+public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener, ScaleGestureDetector.OnScaleGestureListener, View.OnTouchListener {
     private String TAG = getClass().getSimpleName();
     private boolean mOnce = true;
+    private Matrix matrix;
+    private float mInitScale;
+    private float mMidScale;
+    private float mMaxScale;
+    private ScaleGestureDetector gestureDetector;
 
     public ZoomImage(Context context) {
-//        super(context);
         this(context, null);
     }
 
     public ZoomImage(Context context, AttributeSet attrs) {
-//        super(context, attrs);
         this(context, attrs, 0);
         Log.d(TAG, "ZoomImage: attrs=" + attrs.toString());
     }
 
     public ZoomImage(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setScaleType(ScaleType.MATRIX);
+        matrix = new Matrix();
+        gestureDetector = new ScaleGestureDetector(context,this);
+        setOnTouchListener(this);
+        init();
+    }
+
+    private void init() {
+
     }
 
 
@@ -78,13 +94,71 @@ public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLay
             if (dh > height && dw < width)
                 scale = height * 1.0f / dh;
 
-            if ((dw > width && dh < height) && (dh < height && dw < width)) {
+            if ((dw > width && dh > height) || (dh < height && dw < width)) {
                 scale = Math.min(width * 1.0f / dw, height * 1.0f / dh);
             }
+
+            mInitScale = scale;
+            mMidScale = scale * 2;
+            mMaxScale = scale * 4;
+
+            matrix.postTranslate(getWidth()/2 - dw/2,getHeight()/2 - dh /2);
+            matrix.postScale(scale,scale,getWidth() / 2,getHeight()/2);
+            setImageMatrix(matrix);
 
             mOnce = false;
         }
 
 
+    }
+
+    public float getScale(){
+        float[] valuse = new float[9];
+        matrix.getValues(valuse);
+        return valuse[Matrix.MSCALE_X];
+    }
+
+    //缩放的区间：mid,max
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        float scaleFactor = detector.getScaleFactor();
+        Log.d(TAG, "onScale: detector--scaleFactor=" + scaleFactor);
+        if (getDrawable() == null) {
+            return true;
+        }
+        float scale = getScale();
+        if ((scale < mMaxScale && scaleFactor > 1.0f) || (scale > mInitScale && scaleFactor < 1.0f)) {
+
+            if (scale * scaleFactor < mInitScale) {
+                scaleFactor = mInitScale / scale;
+            }
+
+            if (scale * scaleFactor > mMaxScale) {
+                scaleFactor = mMaxScale / scale;
+            }
+
+            matrix.postScale(scaleFactor, scaleFactor, getWidth() / 2, getHeight() / 2);
+            setImageMatrix(matrix);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        Log.d(TAG, "onScaleBegin: detector=" + detector);
+//        return false;//default
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+        Log.d(TAG, "onScaleEnd: detector");
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return true;
     }
 }
