8b8ce0f9e0b84d833f244d4f4189ee0b0385cc6f
yangjiajia-home <576125754@qq.com>
2016-07-31 22:26:19 +0800

    双击放大还没搞定

--- src/com/example/testandroid/activity/ZoomImage.java
+++ src/com/example/testandroid/activity/ZoomImage.java
@@ -1,14 +1,18 @@
 package com.example.testandroid.activity;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
@@ -22,11 +26,24 @@ import android.widget.ImageView;
 public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener, ScaleGestureDetector.OnScaleGestureListener, View.OnTouchListener {
     private String TAG = getClass().getSimpleName();
     private boolean mOnce = true;
-    private Matrix matrix;
+    private Matrix mMatrix;
     private float mInitScale;
     private float mMidScale;
     private float mMaxScale;
-    private ScaleGestureDetector gestureDetector;
+    private ScaleGestureDetector mScaleGestureDetector;
+    //-------------------------------自由移动
+    private int mLastPointerCount;//
+    private float mLastX;//
+    private float mLastY;//
+    private int mTouchSlop;
+    private boolean isCanDrag;
+    //---------------------双击放大与缩小
+
+
+    private boolean isCheckLeftAndRight;
+    private boolean isCheckTopAndBottom;
+
+    GestureDetector mGestureDetector;
 
     public ZoomImage(Context context) {
         this(context, null);
@@ -40,9 +57,39 @@ public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLay
     public ZoomImage(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setScaleType(ScaleType.MATRIX);
-        matrix = new Matrix();
-        gestureDetector = new ScaleGestureDetector(context,this);
+        mMatrix = new Matrix();
+        mScaleGestureDetector = new ScaleGestureDetector(context, this);
         setOnTouchListener(this);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+
+        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                final float x = e.getX();
+                final float y = e.getY();
+
+                if (getScale() < mMidScale) {
+                    ValueAnimator animator = ValueAnimator.ofFloat(getScale(), mMaxScale / getScale());
+                    animator.setDuration(1000);
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            Log.d(TAG, "onAnimationUpdate: animation="+animation.getAnimatedValue());
+//                            mMatrix.postScale((float)animation.getAnimatedValue(),(float)animation.getAnimatedValue(),x,y);
+
+//                            setImageMatrix(mMatrix);
+                        }
+                    });
+                    animator.start();
+
+
+                }else {
+                    mMatrix.postScale(mInitScale/getScale(),mInitScale/getScale(),x,y);
+                    setImageMatrix(mMatrix);
+                }
+                return true;
+            }
+        });
         init();
     }
 
@@ -101,9 +148,9 @@ public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLay
             mMidScale = scale * 2;
             mMaxScale = scale * 4;
 
-            matrix.postTranslate(getWidth()/2 - dw/2,getHeight()/2 - dh /2);
-            matrix.postScale(scale,scale,getWidth() / 2,getHeight()/2);
-            setImageMatrix(matrix);
+            mMatrix.postTranslate(getWidth() / 2 - dw / 2, getHeight() / 2 - dh / 2);
+            mMatrix.postScale(scale, scale, getWidth() / 2, getHeight() / 2);
+            setImageMatrix(mMatrix);
 
             mOnce = false;
         }
@@ -111,9 +158,9 @@ public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLay
 
     }
 
-    public float getScale(){
+    public float getScale() {
         float[] valuse = new float[9];
-        matrix.getValues(valuse);
+        mMatrix.getValues(valuse);
         return valuse[Matrix.MSCALE_X];
     }
 
@@ -136,8 +183,8 @@ public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLay
                 scaleFactor = mMaxScale / scale;
             }
 
-            matrix.postScale(scaleFactor, scaleFactor, getWidth() / 2, getHeight() / 2);
-            setImageMatrix(matrix);
+            mMatrix.postScale(scaleFactor, scaleFactor, getWidth() / 2, getHeight() / 2);
+            setImageMatrix(mMatrix);
         }
 
         return true;
@@ -157,7 +204,116 @@ public class ZoomImage extends ImageView implements ViewTreeObserver.OnGlobalLay
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        gestureDetector.onTouchEvent(event);
+        if (mGestureDetector.onTouchEvent(event))
+            return true;
+
+
+        mScaleGestureDetector.onTouchEvent(event);
+        float x = 0;
+        float y = 0;
+        int pointerCount = event.getPointerCount();
+        for (int i = 0; i < pointerCount; i++) {
+            x += event.getX(i);
+            y += event.getY(i);
+        }
+
+        x = x / pointerCount;
+        y = y / pointerCount;
+
+        if (mLastPointerCount != pointerCount) {
+            isCanDrag = false;
+            mLastX = x;
+            mLastY = y;
+        }
+
+        mLastPointerCount = pointerCount;
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - mLastX;
+                float dy = y - mLastY;
+
+                if (!isCanDrag) {
+                    isCanDrag = isMoveAction(dx, dy);
+                }
+
+                if (isCanDrag) {
+                    RectF rectF = getMatritRectF();
+                    if (getDrawable() != null) {
+
+                        isCheckLeftAndRight = isCheckTopAndBottom = true;
+
+                        if (rectF.width() < getWidth()){
+                            isCheckLeftAndRight = false;
+                            dx =0;
+                        }
+
+                        if (rectF.height() <getHeight()){
+                            isCheckTopAndBottom = false;
+                            dy = 0;
+                        }
+
+                        mMatrix.postTranslate(dx,dy);
+                        checkBorderWhenTranslate();
+                        setImageMatrix(mMatrix);
+                    }
+                }
+                mLastX = x;
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                isCanDrag = false;
+                mLastPointerCount = 0;
+
+                break;
+            default:
+                break;
+        }
+
         return true;
+    }
+
+    private void checkBorderWhenTranslate() {
+        RectF rectF = getMatritRectF();
+        float deltaX = 0;
+        float deltaY = 0;
+
+        int width = getWidth();
+        int height = getHeight();
+
+        if (rectF.top > 0 && isCheckTopAndBottom) {
+            deltaY = -rectF.top;
+        }
+        if (rectF.bottom < height && isCheckTopAndBottom) {
+            deltaY = height - rectF.bottom;
+        }
+///////////
+        if (rectF.left > 0 && isCheckLeftAndRight) {
+            deltaX =  - rectF.left;
+        }
+
+        if (rectF.right < width && isCheckLeftAndRight) {
+            deltaX = width - rectF.right;
+        }
+
+        mMatrix.postTranslate(deltaX,deltaY);
+
+    }
+
+    private RectF getMatritRectF() {
+        Matrix matrix = mMatrix;
+        RectF rectF = new RectF();
+        Drawable drawable = getDrawable();
+        if ((drawable!=null)) {
+            rectF.set(0,0,drawable.getIntrinsicWidth(),drawable.getIntrinsicHeight());
+            matrix.mapRect(rectF);
+        }
+        Log.d(TAG, "getMatritRectF: rectF=" + rectF);
+        return rectF;
+    }
+
+    private boolean isMoveAction(float dx, float dy) {
+        return Math.sqrt(dx * dx + dy * dy) > mTouchSlop;
     }
 }
