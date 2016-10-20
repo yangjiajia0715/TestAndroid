14bfc439e086049938a5c277f64807bb832d195e
yangjiajia-home <576125754@qq.com>
2016-10-20 22:52:41 +0800

    photoview 入口 还没引入库文件

---
+++ src/com/example/testandroid/activity/PhotoViewActivity.java
@@ -0,0 +1,21 @@
+package com.example.testandroid.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.testandroid.R;
+
+/**
+ * author: yangjiajia
+ * create time: 2016/10/20 0020
+ * desc:
+ */
+
+public class PhotoViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_view);
+    }
+}
