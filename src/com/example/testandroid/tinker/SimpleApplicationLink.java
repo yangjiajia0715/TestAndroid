8cb41936ba4d14ef4a564f228c576c0e04b823a9
yangjiajia-company <875880715@qq.com>
2016-10-30 20:46:30 +0800

    copy了一些 管理类，生成application

--- src/com/example/testandroid/tinker/SimpleApplicationLink.java
+++ src/com/example/testandroid/tinker/SimpleApplicationLink.java
@@ -1,11 +1,17 @@
 package com.example.testandroid.tinker;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
+import android.support.multidex.MultiDex;
 
+import com.example.testandroid.Log.MyLogImp;
+import com.example.testandroid.util.SampleApplicationContext;
+import com.example.testandroid.util.TinkerManager;
 import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 import com.tencent.tinker.loader.app.DefaultApplicationLike;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
@@ -20,5 +26,28 @@ import com.tencent.tinker.loader.shareutil.ShareConstants;
 public class SimpleApplicationLink extends DefaultApplicationLike {
     public SimpleApplicationLink(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent, Resources[] resources, ClassLoader[] classLoader, AssetManager[] assetManager) {
         super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent, resources, classLoader, assetManager);
+    }
+
+
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+
+        SampleApplicationContext.application = getApplication();
+        SampleApplicationContext.context = getApplication();
+        TinkerManager.setTinkerApplicationLike(this);
+        TinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        //optional set logIml, or you can use default debug log
+        TinkerInstaller.setLogIml(new MyLogImp());
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerManager.installTinker(this);
     }
 }
