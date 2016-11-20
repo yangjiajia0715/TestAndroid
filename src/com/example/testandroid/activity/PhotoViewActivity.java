22275168bf1de936a5af898e0187f1fc4f15910c
yangjiajia-company <875880715@qq.com>
2016-11-20 20:15:01 +0800

    large-layout

--- src/com/example/testandroid/activity/PhotoViewActivity.java
+++ src/com/example/testandroid/activity/PhotoViewActivity.java
@@ -2,6 +2,7 @@ package com.example.testandroid.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.testandroid.R;
 
@@ -12,10 +13,15 @@ import com.example.testandroid.R;
  */
 
 public class PhotoViewActivity extends Activity {
+    private static final String TAG = "PhotoViewActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_view);
+
+        Log.e(TAG, "onCreate: PhotoViewActivity");
+        Log.d(TAG, "onCreate d: PhotoViewActivity");
+        Log.v(TAG, "onCreate v: PhotoViewActivity");
     }
 }
