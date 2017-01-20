84c40acea677332657ff962d84e26ceaffbf8faf
yangjiajia-company <875880715@qq.com>
2017-01-20 19:02:25 +0800

    AppBarLayout

--- app/src/main/java/com/testandroid/yang/activity/CoordinatorAppBarActivity.java
+++ app/src/main/java/com/testandroid/yang/activity/CoordinatorAppBarActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.widget.NestedScrollView;
 import android.util.Log;
 
@@ -20,7 +21,7 @@ import butterknife.ButterKnife;
 public class CoordinatorAppBarActivity extends BaseActivity {
     private static final String TAG = "CoordinatorAppBarActivi";
 
-//    @BindView(R.id.toolbar)
+    //    @BindView(R.id.toolbar)
 //    Toolbar toolBar;
 //    @BindView(R.id.top_image)
 //    ImageView topImage;
@@ -28,6 +29,8 @@ public class CoordinatorAppBarActivity extends BaseActivity {
     AppBarLayout appBar;
     @BindView(R.id.nested_scrollview)
     NestedScrollView nestedScrollView;
+    @BindView(R.id.collapsingtoolbarlayout)
+    CollapsingToolbarLayout collapsingtoolbarlayout;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, CoordinatorAppBarActivity.class);
@@ -45,18 +48,23 @@ public class CoordinatorAppBarActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+//        ButtonBarLayout
+//        https://github.com/Bilibili/ijkplayer
     }
 
     @Override
     public void initData() {
         Log.d(TAG, "initData: appBar=" + appBar);
         if (appBar != null)
-        appBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                Log.d(TAG, "onOffsetChanged: verticalOffset=" + verticalOffset);
-            }
-        });
+            appBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+                @Override
+                public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                    Log.d(TAG, "onOffsetChanged: verticalOffset=" + verticalOffset);
+                }
+            });
+
+//        if (nestedScrollView != null) {
+//            nestedScrollView.setNestedScrollingEnabled(false);
+//        }
     }
 }
