d83a5924cea883e9e6e90b2293153c679cbabcbc
yangjiajia-mac <875880715@qq.com>
2017-05-09 21:56:45 +0800

    SwipeRefreshLayout

--- app/src/main/java/com/testandroid/yang/activity/VLayoutActivity.java
+++ app/src/main/java/com/testandroid/yang/activity/VLayoutActivity.java
@@ -3,7 +3,9 @@ package com.testandroid.yang.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.alibaba.android.vlayout.DelegateAdapter;
 import com.alibaba.android.vlayout.VirtualLayoutManager;
@@ -15,7 +17,9 @@ import com.testandroid.yang.adapter.HomePagew2Adapter;
  * Created by yangjiajia on 2017/5/8.
  */
 
-public class VLayoutActivity extends BaseActivity {
+public class VLayoutActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener {
+    private static final String TAG = "VLayoutActivity";
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, VLayoutActivity.class);
@@ -28,13 +32,23 @@ public class VLayoutActivity extends BaseActivity {
         setContentView(R.layout.activity_vlayout);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        VirtualLayoutManager layoutManager = new VirtualLayoutManager(this,VirtualLayoutManager.VERTICAL);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayou);
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+//        swipeRefreshLayout.setColorSchemeResources();
+//        swipeRefreshLayout.setProgressBackgroundColorSchemeResource();
+
+
+
+        VirtualLayoutManager layoutManager = new VirtualLayoutManager(this, VirtualLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
         DelegateAdapter delegateAdapter = new DelegateAdapter(layoutManager);
 
         delegateAdapter.addAdapter(new HomePage1Adapter(this));//
         delegateAdapter.addAdapter(new HomePagew2Adapter(this));
+
         recyclerView.setAdapter(delegateAdapter);
 
     }
@@ -48,4 +62,10 @@ public class VLayoutActivity extends BaseActivity {
     public void initData() {
 
     }
+
+    @Override
+    public void onRefresh() {
+        Log.d(TAG, "onRefresh: ");
+    }
+
 }
