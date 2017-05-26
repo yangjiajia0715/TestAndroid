557a7a572113ebf4f8abe3e1697915d442aaa5bd
yangjiajia-mac <875880715@qq.com>
2017-05-26 21:55:09 +0800

    UriMatcher

--- app/src/main/java/com/testandroid/yang/db/ApplicationContentProvider.java
+++ app/src/main/java/com/testandroid/yang/db/ApplicationContentProvider.java
@@ -2,9 +2,11 @@ package com.testandroid.yang.db;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -16,6 +18,7 @@ import android.util.Log;
 
 public class ApplicationContentProvider extends ContentProvider {
     private static final String TAG = "ApplicationContentProvi";
+    private static UriMatcher uriMatcher;
 
     @Override
     public boolean onCreate() {
@@ -30,6 +33,11 @@ public class ApplicationContentProvider extends ContentProvider {
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
 
+        uriMatcher = new UriMatcher(2);
+        uriMatcher.addURI("","",444);
+//        ContactsContract.CommonDataKinds.Contactables;
+        
+        uriMatcher.match(ContactsContract.Contacts.CONTENT_URI);
         return null;
     }
 
