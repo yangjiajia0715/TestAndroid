1deda2fe7596430806179eae825c83dc689e07d3
yangjiajiaCompany <875880715@qq.com>
2017-06-11 20:02:18 +0800

    jdbc java操作数据库，连接池：dbcp d3p0 DbUtil

--- MyJavaProgrammer/src/main/java/com/example/MyClass.java
+++ MyJavaProgrammer/src/main/java/com/example/MyClass.java
@@ -1,11 +1,21 @@
 package com.example;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class MyClass {
-    public static void main(String []args){
+    public static void main(String[] args) throws FileNotFoundException {
 //        System.out.print("yangjiajia");
 ////        System.out.println();
 //        System.out.printf("");
@@ -30,9 +40,62 @@ public class MyClass {
 ////        [A-Za-z0-9_-]*
 //        System.out.printf("toArray toArray=" + videoId.matches("[A-Za-z0-9]*"));
 //        testArrayAddDelNull();
-        testMapAddDelNull();
+//        testMapAddDelNull();
+
+        testJdbc();
+
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ResourceBundle bundle = ResourceBundle.getBundle("jdbc");//不加后缀
+        String url = bundle.getString("url");
+        System.out.print("--------url=" + url);
+    }
+
+    //全部是 java.sql 下的
+    private static void testJdbc() {
+        try {
+            //注册驱动（导包）
+            Class.forName("com.mysql.jdbc.Driver");
+            //获取连接
+            Connection conn = DriverManager.getConnection("jdbc:mysql//localhost:3306/test2", "root", "yangjiajia");
+            //编写sql
+            String sql = "select * from category";
+
+            //创建语句执行着
+            PreparedStatement st = conn.prepareStatement(sql);
+
+            //设置参数
+
+            //执行sql
+            ResultSet resultSet = st.executeQuery();
+
+            //处理结果
+            while (resultSet.next()) {
+                String string = resultSet.getString("");
+                System.out.println("------------------------string=" + string);
+            }
+
+            //释放资源  先创建的后释放
+            resultSet.close();
+
+            st.close();
+            conn.close();
 
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testTest() {
+        System.out.print("------------------");
+        return;
     }
 
     /**
@@ -51,9 +114,9 @@ public class MyClass {
         arrayList.remove(null);
         arrayList.remove(null);
         boolean remove = arrayList.remove(null);
-        boolean remove22 =  arrayList.remove(new TestUserInfo("dfdsf",22));
-        System.out.println("--------------arrayList---------------2--remove=" + remove );
-        System.out.println("--------------arrayList---------------2--remove22=" + remove22 );
+        boolean remove22 = arrayList.remove(new TestUserInfo("dfdsf", 22));
+        System.out.println("--------------arrayList---------------2--remove=" + remove);
+        System.out.println("--------------arrayList---------------2--remove22=" + remove22);
         System.out.println("arrayList arrayList size=" + arrayList.size() + "" + arrayList.toString());
     }
 
