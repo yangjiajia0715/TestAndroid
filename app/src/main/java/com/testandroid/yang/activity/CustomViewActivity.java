f73602a02f27830b85088cb5df4b78477ae43f7c
yangjiajiaCompany <875880715@qq.com>
2017-06-17 23:05:50 +0800

    config changed

--- app/src/main/java/com/testandroid/yang/activity/CustomViewActivity.java
+++ app/src/main/java/com/testandroid/yang/activity/CustomViewActivity.java
@@ -3,7 +3,9 @@ package com.testandroid.yang.activity;
 import android.app.MediaRouteButton;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -94,5 +96,11 @@ public class CustomViewActivity extends BaseActivity {
             case R.id.view10:
                 break;
         }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "onConfigurationChanged: new" + newConfig);
     }
 }
