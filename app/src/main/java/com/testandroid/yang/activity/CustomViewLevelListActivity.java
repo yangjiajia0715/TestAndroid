4ecc77de171342f4b28e53be464b5e785cdce750
yangjiajiaCompany <875880715@qq.com>
2017-06-23 19:07:07 +0800

    LevelListDrawable LayerDrawabler ClipDrawable

--- app/src/main/java/com/testandroid/yang/activity/CustomViewLevelListActivity.java
+++ app/src/main/java/com/testandroid/yang/activity/CustomViewLevelListActivity.java
@@ -1,7 +1,11 @@
 package com.testandroid.yang.activity;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.LevelListDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,6 +37,10 @@ public class CustomViewLevelListActivity extends BaseActivity {
     TextView btnTranstionPreviours;
     @BindView(R.id.btn_transtion_next)
     TextView btnTranstionNext;
+    @BindView(R.id.levellist_layerlayter_clip)
+    ImageView levellistLayerlayterClip;
+    @BindView(R.id.levellist_layerlayter_click)
+    TextView levellistLayerlayterClick;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, CustomViewLevelListActivity.class);
@@ -55,10 +63,12 @@ public class CustomViewLevelListActivity extends BaseActivity {
 
     @Override
     public void initData() {
-
+        levellistLayerlayterClip.setImageLevel(40);//init
     }
 
-    @OnClick({R.id.btn_level_list, R.id.btn_transtion_previours, R.id.btn_transtion_next})
+    @OnClick({R.id.btn_level_list, R.id.btn_transtion_previours,
+            R.id.levellist_layerlayter_click,
+            R.id.btn_transtion_next})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_level_list:
@@ -73,6 +83,26 @@ public class CustomViewLevelListActivity extends BaseActivity {
                 TransitionDrawable transitionDrawable2 = (TransitionDrawable) transtionDrawable.getDrawable();
                 transitionDrawable2.resetTransition();
                 break;
+            case R.id.levellist_layerlayter_click://简化版，实际应用中 需要用instance 端盘
+                levellistLayerlayterClip.setImageLevel(80);
+                LevelListDrawable levelListDrawable = (LevelListDrawable) levellistLayerlayterClip.getDrawable();
+                LayerDrawable layerDrawable = (LayerDrawable) levelListDrawable.getCurrent();
+                int numberOfLayers = layerDrawable.getNumberOfLayers();
+                if (numberOfLayers > 1) {
+                    final ClipDrawable clipDrawable = (ClipDrawable) layerDrawable.getDrawable(numberOfLayers - 1);
+                    ValueAnimator valueAnimator = ValueAnimator.ofInt(0, levelListDrawable.getLevel());
+                    valueAnimator.setDuration(2000);
+                    valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            Integer integer = (Integer) animation.getAnimatedValue();
+                            clipDrawable.setLevel(100 * integer);//0--10000
+                        }
+                    });
+                    valueAnimator.start();
+                }
+                break;
         }
     }
+
 }
