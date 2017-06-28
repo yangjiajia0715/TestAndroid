30242ddd5691be72a7af0c813ba02992979383f9
yangjiajiaCompany <875880715@qq.com>
2017-06-28 19:19:43 +0800

    launchMode

---
+++ app/src/main/java/com/testandroid/yang/activity/LaunchModeBaseActivity.java
@@ -0,0 +1,20 @@
+package com.testandroid.yang.activity;
+
+/**
+ * 测试启动模式
+ * Created by yangjiajia on 2017/6/28.
+ */
+
+public class LaunchModeBaseActivity extends BaseActivity {
+
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
