fe9cb6b88525d382b6ec9992b8d2a8a9407909ac
yangjiajiaCompany <875880715@qq.com>
2017-10-10 19:12:32 +0800

    topic contentprovider

---
+++ app/src/main/java/com/testandroid/yang/provider/TopicContentProvider.java
@@ -0,0 +1,54 @@
+package com.testandroid.yang.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * 添加错题
+ * 考虑是否需要provider
+ * 可能给其他应用使用 需要：
+ *  1，authority
+ *  2, 表结构 合约类 Contract 
+ *  3, SqliteOpenHelper 创建数据库
+ *  4，重写contentProvider
+ * Created by yangjiajia on 2017/10/10.
+ */
+
+public class TopicContentProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
