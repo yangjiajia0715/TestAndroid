8bc396aa936cc2784d51f7813da41f30125fb2ec
yangjiajiaCompany <875880715@qq.com>
2017-12-15 19:16:20 +0800

    file en

--- app/src/main/java/com/testandroid/yang/activity/FileEncryptActivity.java
+++ app/src/main/java/com/testandroid/yang/activity/FileEncryptActivity.java
@@ -5,20 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.testandroid.yang.R;
 
-import org.apache.commons.io.FileUtils;
-
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import butterknife.BindView;
@@ -37,6 +36,8 @@ public class FileEncryptActivity extends BaseActivity implements AdapterView.OnI
     ListView mListview;
     private List<String> mdates = new ArrayList<>();
     private ArrayAdapter<String> mAdapter;
+    private static final String FILE_SUFFIX = ".encrypion";
+    private String mCurPath;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, FileEncryptActivity.class);
@@ -60,12 +61,15 @@ public class FileEncryptActivity extends BaseActivity implements AdapterView.OnI
                 finish();
             }
         });
+        setSupportActionBar(mToolbar);
 
         mToolbar.setTitle("文件列表");
         mListview.setOnItemClickListener(this);
 
+
         File directory = Environment.getExternalStorageDirectory();
-        Collection<File> files = FileUtils.listFiles(directory, null, false);
+        File[] files = directory.listFiles();
+//        Collection<File> files = FileUtils.listFiles(directory, null, false);
         for (File file : files) {
             mdates.add(file.getPath());
         }
@@ -83,7 +87,26 @@ public class FileEncryptActivity extends BaseActivity implements AdapterView.OnI
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_file_encrypy:
-
+                if (!TextUtils.isEmpty(mCurPath)) {
+                    File file = new File(mCurPath);
+                    if (file.isFile()) {
+                        if (!file.getParent().endsWith(FILE_SUFFIX)) {
+                            File newfile = new File(file.getPath() + FILE_SUFFIX);
+                            file.renameTo(newfile);
+                        }
+                    }
+                }
+                return true;
+            case R.id.action_file_decode:
+                if (!TextUtils.isEmpty(mCurPath)) {
+                    File file = new File(mCurPath);
+                    if (file.isFile() && file.getParent().endsWith(FILE_SUFFIX)) {
+                        String path = file.getPath();
+                        String substring = path.substring(0, path.indexOf(FILE_SUFFIX));
+                        File newfile = new File(substring);
+                        file.renameTo(newfile);
+                    }
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -96,10 +119,15 @@ public class FileEncryptActivity extends BaseActivity implements AdapterView.OnI
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String path = mdates.get(position);
-        File fileP = new File(path);
+        mCurPath = mdates.get(position);
+        File fileP = new File(mCurPath);
+        if (fileP.isFile()) {
+            Toast.makeText(this, "文件", Toast.LENGTH_SHORT).show();
+            return;
+        }
         mdates.clear();
-        Collection<File> files = FileUtils.listFiles(fileP, null, false);
+//        Collection<File> files = FileUtils.listFiles(fileP, null, false);
+        File[] files = fileP.listFiles();
         for (File file : files) {
             mdates.add(file.getPath());
         }
